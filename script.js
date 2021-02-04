d2dedee7a68e542950104759808255fc0aeacdd8
Hossain <hossainrabbi259@gmail.com>
2021-02-04 14:24:26 +0600

    set default value and error message

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function weatherSearch() {
+function weather() {
   const cityInput = document.getElementById('city-input');
+  let cityInputValue = cityInput.value;
+  if (cityInputValue.length == 0) {
+    cityInput.value = 'dhaka';
+  }
   const cityName = cityInput.value.toLowerCase();
   const apiKey = '6d8064244f3dd90c850da59c262d3fc8';
   const API = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
@@ -10,10 +14,15 @@ function weatherSearch() {
       document.getElementById('current-city-name').innerText = data.name;
       document.getElementById('current-temp').innerText = temp;
       document.getElementById('current-louds').innerText = data.weather[0].main;
+      document.getElementById('error-message').style.display = 'none';
       cityInput.value = '';
     })
     .catch((err) => {
-      alert('Please give me a valid city name');
+      document.getElementById('error-message').style.display = 'block';
       cityInput.value = '';
     });
+}
+weather();
+function weatherSearch() {
+  weather();
 }
